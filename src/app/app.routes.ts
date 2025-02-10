a680f1b9eb1546266efe306895a2d1fe453419a3
Kassianyy <githubcris@gmail.com>
2025-02-09 19:52:44 -0500

    se agrego vistas de login, espera,resgistro.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,16 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./views/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./views/sign-up/sign-up.page').then( m => m.SignUpPage)
+  },
+  {
+    path: 'waiting',
+    loadComponent: () => import('./views/waiting/waiting.page').then( m => m.WaitingPage)
+  },
 ];
