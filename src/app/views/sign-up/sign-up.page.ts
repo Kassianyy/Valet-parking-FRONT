f23b9b2f2c9f04981108c9b7eae56db23e77c318
Kassianyy <githubcris@gmail.com>
2025-02-14 22:12:07 -0500

    se agrego contenido al registro de usuario

--- src/app/views/sign-up/sign-up.page.ts
+++ src/app/views/sign-up/sign-up.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonInput, IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
   styleUrls: ['./sign-up.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonImg, IonInput, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class SignUpPage implements OnInit {
 
