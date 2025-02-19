d0fab441d4f0ff570b70297f44d0740e22e86b6c
Kassianyy <githubcris@gmail.com>
2025-02-19 18:09:10 -0500

    se agrego conteniudo front a la vista de espera y contenido a la vista de registro

--- src/app/views/reg-vehiculo/reg-vehiculo.page.ts
+++ src/app/views/reg-vehiculo/reg-vehiculo.page.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonImg, IonBackButton, IonButton, IonIcon, IonInput, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonMenuButton, IonImg, IonBackButton, IonButton, IonIcon, IonInput, IonLabel, IonItem, IonList, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-reg-vehiculo',
   templateUrl: './reg-vehiculo.page.html',
   styleUrls: ['./reg-vehiculo.page.scss'],
   standalone: true,
-  imports: [IonList, IonItem, IonLabel, IonInput, IonIcon, IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonImg]
+  imports: [IonList, IonItem, IonLabel, IonInput, IonIcon, IonButton, IonBackButton, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, IonImg, IonSelect, IonSelectOption]
 })
 export class RegVehiculoPage {
   searchText: string = '';
   brands: string[] = ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'Nissan', 'Mazda', 'Hyundai', 'Kia', 'Volkswagen', 'BMW'];
   filteredBrands: string[] = [];
+  tipoUsuario: string = ''; // Variable para la selección de "Socio" o "Invitado"
 
   constructor() {}
 
--- src/app/views/waiting/waiting.page.ts
+++ src/app/views/waiting/waiting.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader,IonMenuButton, IonTitle, IonToolbar, IonImg, IonButtons, IonList, IonAvatar, IonItem, IonLabel, IonButton, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-waiting',
   templateUrl: './waiting.page.html',
   styleUrls: ['./waiting.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonButton, IonLabel, IonItem, IonAvatar, IonList, IonButtons, IonImg, IonContent, IonHeader,IonMenuButton, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class WaitingPage implements OnInit {
 
